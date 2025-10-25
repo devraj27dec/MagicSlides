aca92f86c0c5337b38424769ed1d83f0cd5dc1f0
Devraj Rajput <rajkirar2002.pc@gmail.com>
2025-10-25 07:55:03 +0530

    Project-Initialized🚀

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center p-2 text-3xl">
+      Hello! Welcome to MagicSlides App🚀
+    </div>
+  );
+}
